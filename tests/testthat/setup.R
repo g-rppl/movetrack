1e554f235b280615fa694368a260675c3522410f
Georg Rüppel <g-rppl@posteo.de>
2024-06-04 13:51:06 +0200

    set cmdstan path in tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,6 +2,8 @@ data(motusData)
 suppressMessages(loc <- locate(motusData, dTime = 10))
 suppressMessages(loc$ID[1] <- 1)
 
+cmdstanr::set_cmdstan_path()
+
 test_that("track warnings", {
   expect_warning(
     fit <<- track(loc, seed = 42, parallel_chains = 4, refresh = 0)
